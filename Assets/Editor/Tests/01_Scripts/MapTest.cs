397ce67d02cefd13009a091e14bfa0844ded1cd3
Stefano Gioia <stefanogch@gmail.com>
2016-07-11 16:42:48 -0400

    deleted MapTest

--- Assets/Editor/Tests/01_Scripts/MapTest.cs
+++ Assets/Editor/Tests/01_Scripts/MapTest.cs
@@ -4,27 +4,5 @@ using UnityEngine;
 [TestFixture()]
 public class MapTest
 {
-    private Map instance;
-    [SetUp()]
-    public void Init()
-    {
-        
-    }
 
-    [Test()]
-    public void MapMake_throwsException_onEmptyArray()
-    {
-        Assert.Throws<Map.InvalidTileSizeException>(() => instance.MakeMap(new Tile[3][][]));
-    }
-
-    [Test()]
-    public void MapMake_throwsException_onJaggedArray()
-    {
-        Tile[][] tiles = new Tile[3][];
-        tiles[0] = new Tile[3];
-        tiles[1] = new Tile[2];
-        tiles[2] = new Tile[4];
-
-        Assert.Throws<Map.InvalidTileSizeException>(() => instance.MakeMap(tiles,0));
-    }
 }
