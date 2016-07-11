431045611cad5dc9e9a661ac16e20073245777c0
Stefano Gioia <stefanogch@gmail.com>
2016-07-11 17:02:36 -0400

    refactoring Map.cs

--- Assets/01_Scripts/Map.cs
+++ Assets/01_Scripts/Map.cs
@@ -39,46 +39,88 @@ public class Map : MonoBehaviour
         }
     }
 
-    public void MakeMap(Tile[][] tiles,int y)
+    public void MakeMap(Tile[][] tiles, int y)
     {
-        if (tiles == null 
-            || tiles.Any(x => x == null) 
-            || tiles.Any(x => x.Length <= 0) 
+        if (tiles == null
+            || tiles.Any(x => x == null)
+            || tiles.Any(x => x.Length <= 0)
             || Array.Exists(tiles, x => x.Length != tiles[0].Length))
         {
             throw new InvalidTileSizeException("Tiles array must be non-empty rectangular shaped");
         }
-        //New meshes need to be inside a gameObject
-        GameObject meshHolder = new GameObject();
-        MeshFilter subMesh = meshHolder.AddComponent<MeshFilter>();
-        MeshRenderer renderer = meshHolder.AddComponent<MeshRenderer>();
-        renderer.material = m_material;
-        meshHolder.transform.SetParent(transform);
-        meshHolder.transform.localPosition = new Vector3(0, 0, 0);
-
         m_tiles = tiles;
         int tilesHeight = m_tiles.Length;
         int tilesWidth = m_tiles[0].Length;
-        m_worldWidth = tilesWidth * c_tileSize;
-        m_worldHeight = tilesHeight * c_tileSize;
         int verticesWidth = tilesWidth + 1;
         int verticesHeight = tilesHeight + 1;
         int numVertices = verticesWidth * verticesHeight;
+        m_worldWidth = tilesWidth * c_tileSize;
+        m_worldHeight = tilesHeight * c_tileSize;
+
+        Vector3[] mapVertices = CreateVertices(y, verticesWidth, numVertices);
+        int[] triangles = CreateTriangles(verticesWidth, verticesHeight);
+        Vector3[] normals = CreateNormals(numVertices);
+        Vector2[] uvCoords = CreateUVs(tilesHeight, tilesWidth, verticesWidth, numVertices);
+
+        SetMeshFilterProperties(
+            CreateChildMeshHolder().GetComponent<MeshFilter>(), 
+            mapVertices, 
+            uvCoords, 
+            triangles, 
+            normals);
+    }
+
+    private static Vector3[] CreateVertices(int y, int verticesWidth, int numVertices)
+    {
         Vector3[] mapVertices = new Vector3[numVertices * 4];
         //Vertices
         for (int i = 0; i < numVertices; i++)
         {
             for (int j = 0; j < 4; j++)
             {
-                mapVertices[i * 4 + j] = 
-                    new Vector3(i % verticesWidth * c_tileSize, y * c_tileHeight, i / verticesWidth * c_tileSize);
+                mapVertices[i * 4 + j] = new Vector3(i % verticesWidth * c_tileSize,
+                                                     y * c_tileHeight,
+                                                     i / verticesWidth * c_tileSize);
             }
         }
-        int[] triangles = CreateTriangles(verticesWidth, verticesHeight);
-        Vector3[] normals = CreateNormals(numVertices);
-        Vector2[] uvCoords = CreateUVs(tilesHeight, tilesWidth, verticesWidth, numVertices);
 
-        subMesh.mesh.vertices = mapVertices;
+        return mapVertices;
+    }
+
+    private static Vector3[] CreateNormals(int numVertices)
+    {
+        Vector3[] normals = new Vector3[numVertices * 4];
+        for (int i = 0; i < numVertices * 4; i++)
+        {
+            normals[i] = Vector3.up;
+        }
+        return normals;
+    }
+
+    private static int[] CreateTriangles(int verticesWidth, int verticesHeight)
+    {
+        int triIndex = 0;
+        int[] triangles = new int[2 * 3 * verticesWidth * verticesHeight];
+        for (int i = 0; i < verticesHeight - 1; i++)
+        {
+            for (int j = 0; j < verticesWidth - 1; j++)
+            {
+                //Sup tri
+                triangles[triIndex++] = ((i + 1) * verticesWidth + j) * 4 + 0;
+                triangles[triIndex++] = (i * verticesWidth + j + 1) * 4 + 3;
+                triangles[triIndex++] = (i * verticesWidth + j) * 4 + 0 + 2;
+                //Lower tri
+                triangles[triIndex++] = ((i + 1) * verticesWidth + j) * 4 + 0;
+                triangles[triIndex++] = ((i + 1) * verticesWidth + j + 1) * 4 + 1;
+                triangles[triIndex++] = (i * verticesWidth + j + 1) * 4 + 3;
+            }
+        }
+        return triangles;
+    }
+
+    private static void SetMeshFilterProperties(MeshFilter subMesh, Vector3[] vertices, Vector2[] uvCoords, int[] triangles, Vector3[] normals)
+    {
+        subMesh.mesh.vertices = vertices;
         subMesh.mesh.uv = uvCoords;
         subMesh.mesh.triangles = triangles;
         subMesh.mesh.normals = normals;
@@ -105,35 +147,15 @@ public class Map : MonoBehaviour
         return uvCoords;
     }
 
-    private static Vector3[] CreateNormals(int numVertices)
+    private GameObject CreateChildMeshHolder()
     {
-        Vector3[] normals = new Vector3[numVertices * 4];
-        for (int i = 0; i < numVertices * 4; i++)
-        {
-            normals[i] = Vector3.up;
-        }
-        return normals;
-    }
-
-    private static int[] CreateTriangles(int verticesWidth, int verticesHeight)
-    {
-        int triIndex = 0;
-        int[] triangles = new int[2 * 3 * verticesWidth * verticesHeight];
-        for (int i = 0; i < verticesHeight - 1; i++)
-        {
-            for (int j = 0; j < verticesWidth - 1; j++)
-            {
-                //Sup tri
-                triangles[triIndex++] = ((i + 1) * verticesWidth + j) * 4 + 0;                
-                triangles[triIndex++] = (i * verticesWidth + j + 1) * 4 + 3;
-                triangles[triIndex++] = (i * verticesWidth + j) * 4 + 0 + 2;
-                //Lower tri
-                triangles[triIndex++] = ((i + 1) * verticesWidth + j) * 4 + 0;             
-                triangles[triIndex++] = ((i + 1) * verticesWidth + j + 1) * 4 + 1;
-                triangles[triIndex++] = (i * verticesWidth + j + 1) * 4 + 3;
-            }
-        }
-        return triangles;
+        GameObject meshHolder = new GameObject();
+        meshHolder.AddComponent<MeshFilter>();
+        MeshRenderer renderer = meshHolder.AddComponent<MeshRenderer>();
+        renderer.material = m_material;
+        meshHolder.transform.SetParent(transform);
+        meshHolder.transform.localPosition = new Vector3(0, 0, 0);
+        return meshHolder;
     }
 
     private void DestroyChildrenGameObjects()
